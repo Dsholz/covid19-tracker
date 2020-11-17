9f903f37071651db6897bdbf1388f73ccb0b62be
Soladoye Daniel <Soladoyedaniel@gmail.com>
2020-11-17 22:43:48 +0100

    removed wrnings from countrylist component

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { getCovidHistoricalCountriesData } from "../Api";
+import React from "react";
 
 function CountryList(props) {
   const {
